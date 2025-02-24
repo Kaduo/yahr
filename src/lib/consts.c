fb7098309865e4fa9fcd018c0dd213eff2b1336f
Balthazar Potet <balthazarpotet@gmail.com>
2025-02-24 15:53:41 +0100

    This message wasn't typed by a human.

--- src/lib/consts.c
+++ src/lib/consts.c
@@ -120,7 +120,7 @@ const char *TABLET_IP = "192.168.1.24";
 const char *TABLET_IP = "localhost";
 #endif
 
-const char *TABLET_PORT = "27610";
+const char *TABLET_PORT = "37610";
 
 const Signal IMPULSE_SIGNAL = {
     .amplitude = 255,
