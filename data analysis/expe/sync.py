ba53d7f1cf84d0f0ec4fdd6fabbda5403a7e27df
Balthazar Potet <balthazarpotet@gmail.com>
2025-04-16 13:58:18 +0200

    This message wasn't typed by a human.

--- data analysis/expe/sync.py
+++ data analysis/expe/sync.py
@@ -26,11 +26,12 @@ def get_end_time_of_tap(filename):
     else:
         return None
 
-def crop_times_to_problem(name, problem_id):
-    eye_tracking = pl.read_csv(f"data/experiment/results/{name}/{name}.csv")
+def crop_times_to_problem(name, problem_id, haptics):
+    haptics_folder = "haptics" if haptics else "no_haptics"
+    eye_tracking = pl.read_csv(f"data/experiment/results/{haptics_folder}/{name}/{name}.csv")
     eye_tracking = eye_tracking.with_columns((eye_tracking["Timestamp (ms)"]/1000).alias("Timestamp (s)"))
-    start = get_start_time_of_tap(f"data/experiment/results/{name}/problem_{problem_id}.tap")
-    end = get_end_time_of_tap(f"data/experiment/results/{name}/problem_{problem_id}.tap")
+    start = get_start_time_of_tap(f"data/experiment/results/{haptics_folder}/{name}/problem_{problem_id}.tap")
+    end = get_end_time_of_tap(f"data/experiment/results/{haptics_folder}/{name}/problem_{problem_id}.tap")
     print(end - start)
     df = eye_tracking.filter((eye_tracking["Timestamp (s)"] <= end) & (start <= eye_tracking["Timestamp (s)"]))
     df = df.with_columns((df["Timestamp (s)"] - df["Timestamp (s)"][0]).alias("Elapsed time (s)"))
