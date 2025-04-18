698888ba55feb20c6a19881a11440cc3c0e1622f
Balthazar Potet <balthazarpotet@gmail.com>
2025-04-18 10:36:15 +0200

    This message wasn't typed by a human.

--- data analysis/expe/plumbing.py
+++ data analysis/expe/plumbing.py
@@ -9,6 +9,7 @@ TABLET_HEIGHT = 600
 
 UNIT_ROD_HEIGHT = 30
 
+
 class Rod:
     def __init__(self, n=-1):
         self.n = n
@@ -16,12 +17,13 @@ class Rod:
 
 def is_raw_grouping_consistent(grouping):
     seen = [rod.n for rod in grouping[0]]
-    for i in range(1,4):
+    for i in range(1, 4):
         for j in [rod.n for rod in grouping[i]]:
             if j in seen:
                 return False
         seen += [rod.n for rod in grouping[i]]
     return True
+
 
 def _dist(grouping1, grouping2):
     res = 0
@@ -31,12 +33,12 @@ def _dist(grouping1, grouping2):
                 res += 1
     return res
 
-def dist(grouping1, grouping2):
 
+def dist(grouping1, grouping2):
     # if not (is_raw_grouping_consistent(grouping1) and is_raw_grouping_consistent(grouping2)):
     #     return None
 
-    current_grouping  = deepcopy(grouping1)
+    current_grouping = deepcopy(grouping1)
 
     # best_perm = deepcopy(current_grouping)
     best_dist = _dist(current_grouping, grouping2)
@@ -51,29 +53,31 @@ def dist(grouping1, grouping2):
     return best_dist
 
 
-ideal_grouping_ns = [[2, 4, 8], [3, 6, 9], [1,7], [5,10]]
+ideal_grouping_ns = [[2, 4, 8], [3, 6, 9], [1, 7], [5, 10]]
 
 
 def score_grouping(grouping):
     return dist(grouping, [[Rod(k) for k in group] for group in ideal_grouping_ns])
 
+
 def which_zone(x, y):
-    if y <= TABLET_HEIGHT/2 - UNIT_ROD_HEIGHT:
+    if y <= TABLET_HEIGHT / 2 - UNIT_ROD_HEIGHT:
         # print("out y :", y)
         return None
-    if x < TABLET_WIDTH/2:
-        if y < 3*TABLET_HEIGHT/4:
+    if x < TABLET_WIDTH / 2:
+        if y < 3 * TABLET_HEIGHT / 4:
             return 0
         else:
             return 1
-    elif TABLET_WIDTH/2 <= x:
-        if y < 3*TABLET_HEIGHT/4:
+    elif TABLET_WIDTH / 2 <= x:
+        if y < 3 * TABLET_HEIGHT / 4:
             return 2
         else:
             return 3
-    
+
+
 def get_grouping_from_file(filename):
-    grouping = [[],[],[],[], 0]
+    grouping = [[], [], [], [], 0]
     rods = []
     can_be_three = False
     current_rod_is_done = False
@@ -92,13 +96,13 @@ def get_grouping_from_file(filename):
             elif line[0] == "rect":
                 x = float(line[1])
                 y = float(line[2])
-                if which_zone(x,y) is not None:
+                if which_zone(x, y) is not None:
                     grouping[which_zone(x, y)].append(rods[-1])
                 else:
                     grouping[4] += 1
                 width = float(line[4])
-                if width != 3*UNIT_ROD_HEIGHT:
-                    rods[-1].n = int(width//UNIT_ROD_HEIGHT)
+                if width != 3 * UNIT_ROD_HEIGHT:
+                    rods[-1].n = int(width // UNIT_ROD_HEIGHT)
                     current_rod_is_done = True
                 else:
                     can_be_three = True
@@ -146,13 +150,14 @@ def get_grouping_from_file(filename):
                 elif period == 20:
                     rods[-1].n = [2, 4, 8][i]
                 elif period == 70:
-                    rods[-1].n = [3,6,9][i]
+                    rods[-1].n = [3, 6, 9][i]
                 elif period == 200:
-                    rods[-1].n = [5,10][i]
+                    rods[-1].n = [5, 10][i]
 
     # print("hmm : ", filename, grouping[4])
     # print("ohwouw : ", filename, len([x for x in grouping[:4] if len(x) > 0]))
     return grouping
+
 
 def get_groupings_in_folder(folder_path):
     groupings = []
@@ -162,12 +167,14 @@ def get_groupings_in_folder(folder_path):
 
     return groupings
 
+
 def display_grouping(grouping):
     for i, group in enumerate(grouping):
         if i < 4:
             print(f"group {i} : {[rod.n for rod in group]}")
         else:
             print("nombre de réglettes en dehors de tout groupe :", group)
+
 
 def get_scores_for_user(user_id):
     scores_visual = []
@@ -182,8 +189,8 @@ def get_scores_for_user(user_id):
         grouping = get_grouping_from_file(filename)
         scores_visual.append(score_grouping(grouping))
 
-    
     return scores_haptic, scores_visual
+
 
 def get_scores_for_total_user(user_id):
     scores_total = []
@@ -191,8 +198,9 @@ def get_scores_for_total_user(user_id):
         filename = f"data/pre-test/results/combined/user_{user_id}/session_{i}.rods"
         grouping = get_grouping_from_file(filename)
         scores_total.append(score_grouping(grouping))
-    
+
     return scores_total
+
 
 def get_durations_from_files(filename):
     res = []
@@ -202,6 +210,7 @@ def get_durations_from_files(filename):
             res.append(float(line))
     return res
 
+
 class Grouping:
     def __init__(self, filename):
         self.raw_grouping = get_grouping_from_file(filename)
@@ -210,31 +219,38 @@ class Grouping:
     def nb_inconsistencies(self):
         res = 0
         seen = deepcopy(self.inner[0])
-        for i in range(1,4):
+        for i in range(1, 4):
             for n in self.inner[i]:
                 if n in seen:
                     res += 1
             seen += deepcopy(self.inner[i])
         return res
 
-    
     # A grouping is consistent if identical rods are grouped together
     def is_consistent(self):
         return self.nb_inconsistencies() == 0
 
     def score(self):
         return score_grouping(self.raw_grouping)
-    
-class User:
 
+
+class User:
     def __init__(self, user_id, mode):
         self.user_id = user_id
         self.mode = mode
         self.groupings = []
         for i in range(5):
-            self.groupings.append(Grouping(f"data/experiment/results/{mode}/{user_id}/problem_{i}_end.rods"))
-        for i in range(15,20):
-            self.groupings.append(Grouping(f"data/experiment/results/{mode}/{user_id}/problem_{i}_end.rods"))
+            self.groupings.append(
+                Grouping(
+                    f"data/experiment/results/{mode}/{user_id}/problem_{i}_end.rods"
+                )
+            )
+        for i in range(15, 20):
+            self.groupings.append(
+                Grouping(
+                    f"data/experiment/results/{mode}/{user_id}/problem_{i}_end.rods"
+                )
+            )
 
         # try:
         #     self.durations = get_durations_from_files(f"data/experiment-test/results/{mode}/user_{user_id}/user{user_id}_durations")
@@ -248,22 +264,24 @@ class User:
             res.append(grouping.score())
 
         return res
-    
+
     def inconsistency_score(self):
         res = 0
         tot = 0
         for i in range(10):
-            for j in range(i+1, 10):
+            for j in range(i + 1, 10):
                 tot += 1
-                res += dist(self.groupings[i].raw_grouping, self.groupings[j].raw_grouping)
+                res += dist(
+                    self.groupings[i].raw_grouping, self.groupings[j].raw_grouping
+                )
         if tot == 0:
             None
         else:
-            return res/tot
-    
+            return res / tot
+
     def score(self):
         return ma_mean([grouping.score() for grouping in self.groupings])
-    
+
     def nb_inconsistent_groupings(self):
         res = 0
         for grouping in self.groupings:
@@ -271,6 +289,7 @@ class User:
                 res += 1
 
         return res
+
 
 def get_users(mode):
     users = []
@@ -280,11 +299,12 @@ def get_users(mode):
         user_ids = ["ecma3", "emma", "enzo", "mathis", "romain"]
     else:
         raise f"Unkown mode : {mode}"
-    
+
     for id in user_ids:
         users.append(User(id, mode))
-    
+
     return users
+
 
 def ma_sum(l):
     res = 0
@@ -293,6 +313,7 @@ def ma_sum(l):
             res += k
     return res
 
+
 def ma_len(l):
     res = 0
     for k in l:
@@ -300,13 +321,15 @@ def ma_len(l):
             res += 1
     return res
 
+
 def ma_mean(l):
     if l is None:
         return None
     if ma_len(l) > 0:
-        return ma_sum(l)/ma_len(l)
+        return ma_sum(l) / ma_len(l)
     else:
         return None
+
 
 if __name__ == "__main__":
     modes = ["haptics", "no_haptics"]
@@ -314,10 +337,30 @@ if __name__ == "__main__":
         print(mode)
         users = get_users(mode)
         for user in users:
-            print("user_id : ", user.user_id, "| score : ", user.score(), " | inconsistency score : ", user.inconsistency_score(), " | duration : ", ma_mean(user.durations),  " | nb_inconsistent_groupings : ", user.nb_inconsistent_groupings())
-        print("mean score : ", ma_mean([user.score() for user in users]), "| mean inconsistency score : ", ma_mean([user.inconsistency_score() for user in users]),
-              "| mean duration : ", ma_mean([ma_mean(user.durations) for user in users]), "| mean nb_inc_groupings : ", ma_mean([user.nb_inconsistent_groupings() for user in users]))
-        
+            print(
+                "user_id : ",
+                user.user_id,
+                "| score : ",
+                user.score(),
+                " | inconsistency score : ",
+                user.inconsistency_score(),
+                " | duration : ",
+                ma_mean(user.durations),
+                " | nb_inconsistent_groupings : ",
+                user.nb_inconsistent_groupings(),
+            )
+
+        print(
+            "mean score : ",
+            ma_mean([user.score() for user in users]),
+            "| mean inconsistency score : ",
+            ma_mean([user.inconsistency_score() for user in users]),
+            "| mean duration : ",
+            ma_mean([ma_mean(user.durations) for user in users]),
+            "| mean nb_inc_groupings : ",
+            ma_mean([user.nb_inconsistent_groupings() for user in users]),
+        )
+
     # print(ttest_ind([user.score() for user in get_users("combined")], [user.score() for user in get_users("visual")]))
     # print(ttest_ind([user.inconsistency_score() for user in get_users("combined")], [user.inconsistency_score() for user in get_users("haptic")]))
 
--- data analysis/expe/sync.py
+++ data analysis/expe/sync.py
@@ -1,5 +1,6 @@
 import polars as pl
 import plotly.express as px
+import plotly.graph_objects as go
 
 def get_filename_of_tap(name, problem_id, haptics):
     haptics_folder = "haptics" if haptics else "no_haptics"
@@ -24,6 +25,34 @@ def crop_times_to_problem(name, problem_id, haptics):
     df = df.with_columns((df["Timestamp (s)"] - df["Timestamp (s)"][0]).alias("Elapsed time (s)"))
     return df
 
+def get_problem_duration(name, problem_id, haptics):
+    return get_end_time_of_tap(name, problem_id, haptics) - get_start_time_of_tap(name, problem_id, haptics)
+
+def get_touching_boundaries_from_tap(name, problem_id, haptics):
+    res_list = []
+    filename = get_filename_of_tap(name, problem_id, haptics)
+    touching = False
+    # absolute_start_time = get_start_time_of_tap(name, problem_id, haptics)
+    latest_time = None
+    relative_start_time = None
+    with open(filename, "r") as f:
+        for line in f:
+            split_line = line.split()
+            if split_line[0] == "leftMouseButtonFlipped":
+                touching = not touching
+                if touching:
+                    res_list.append([latest_time])
+                else:
+                    res_list[-1].append(latest_time)
+            if split_line[0] == "time":
+                if latest_time is None:
+                    latest_time = 0
+                    relative_start_time = float(split_line[1])
+                else:
+                    latest_time = float(split_line[1]) - relative_start_time
+
+    return res_list
+
 # if __name__ == "__main__":
 #     name = "louis"
 #     problem_id = 4
@@ -31,10 +60,14 @@ def crop_times_to_problem(name, problem_id, haptics):
 #     print(crop_times_to_problem(name, problem_id, True))
 #     fig.show()
 
-def get_problem_duration(name, problem_id, haptics):
-    return get_end_time_of_tap(name, problem_id, haptics) - get_start_time_of_tap(name, problem_id, haptics)
 
 if __name__ == "__main__":
-    name = "romain"
-    for i in range(10):
-        print(get_problem_duration(name, i, False))
+    name = "ecma2"
+    problem_id = 6
+    haptics = True
+    touching_bounds = get_touching_boundaries_from_tap(name, problem_id, haptics)
+    fig = px.line(crop_times_to_problem(name, problem_id, haptics), x="Elapsed time (s)", y="Pitch")
+    for bounds in touching_bounds:
+        fig.add_vrect(bounds[0], bounds[1], opacity=0.5, fillcolor="LightSalmon", line_width=0, layer="below")
+    # fig = px.
+    fig.show()
